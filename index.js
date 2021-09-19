de25c67e606c4556cf420809680bbee0a5b0fd03
Gavriel Frant <gavril44@gmail.com>
2021-09-20 01:11:46 +0300

    added qout api

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 const sendButton = document.getElementById("send-button");
 
+let mainSection = document.getElementById("main");
+
+let sentimData;
+
 sendButton.onclick = async function () {
   textToSend = document.querySelector(".main-input").value;
 
   const response = fetch("https://sentim-api.herokuapp.com/api/v1/", {
-      method: 'POST',
-      headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-      },
-      body: JSON.stringify({'text': textToSend})
-      
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text: textToSend }),
   })
-  .finally(() => stopLoadingAnimation())
-  .then(response => response.json())
-  .then(data => renderAnswer(data))
-  .catch(error => console.log(error))
+    .then((response) => response.json())
+    .then((data) => {sentimData = data; return fetchQuote(data)})
+    .then(response => response.json())
+    .then(data => renderAnswer(sentimData, data))
+    .catch((error) => renderError(error));
 
-  startLoadAnimation()
-
-
+  startLoadAnimation();
 };
 
 function startLoadAnimation() {
+  removeAllChildNodes(mainSection);
 
+  mainSection.style.height = "50%";
+
+  let barDiv = document.createElement("div");
+  barDiv.classList.add("bar");
+
+  let circleDiv = document.createElement("div");
+  circleDiv.classList.add("circle");
+
+  let p = document.createElement("p");
+  p.innerHTML = "Loading";
+  p.classList.add("loading-p");
+
+  barDiv.append(circleDiv, p);
+  mainSection.append(barDiv);
 }
 
-function stopLoadingAnimation() {
+function renderAnswer({result: {polarity}}, quot) {
+  console.log(polarity, quot[getRandomIndx(quot.length)]);
+}
 
+function fetchQuote() {
+  console.log('ddd');
+  // if (polarity > 0) {
+    return fetch("https://type.fit/api/quotes")
+  // }
+}
+
+function renderError() {}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function clearMainSection() {
+  removeAllChildNodes(mainSection);
+}
+
+function getRandomIndx(size) {
+  return Math.floor(Math.random() * size)
 }
